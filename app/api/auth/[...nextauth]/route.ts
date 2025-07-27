fc972c75821a986e3e658dfe4791da4bdf3f86ed
nicolascossi <77031613+nicolascossi@users.noreply.github.com>
2025-07-27 19:04:11 -0300

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,11 @@ import GoogleProvider from "next-auth/providers/google"
 import { FirestoreAdapter } from "@auth/firebase-adapter"
 import { adminDb } from "@/lib/firebase-admin"
 
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
+console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
+console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
+
 // Define el tipo correcto para las opciones de autenticación
 export const authOptions: NextAuthOptions = {
   providers: [
